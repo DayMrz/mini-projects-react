184ee054b06d610a91adf0e9de4db4a775c53d14
Leily <leilymrz@gmail.com>
2022-04-18 21:54:49 -0700

    class components

--- mini-pokedex/src/App.js
+++ mini-pokedex/src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Pokecard from './components/Pokecard'
+import Pokedex from './components/Pokedex'
 
 function App() {
   return (
     <div className="App">
       <h1>Pokedex</h1>
+      <Pokedex />
     </div>
   );
 }
